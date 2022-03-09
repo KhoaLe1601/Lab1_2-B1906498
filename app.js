93c61685cac4171a52d1fd3cd8fb895ad82c14a6
Khoa Le <khoab1906498@example.com>
2022-03-09 14:02:48 +0700

    Dinh nghia cac route cho tai nguyen contact

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const setupContactRoutes = require("./app/routes/contact.router");
 
 app.use(cors());
 app.use(express.json());
-
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application."});
 });
+
+setupContactRoutes(app);
 
 module.exports = app;
